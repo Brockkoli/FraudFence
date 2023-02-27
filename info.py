44b5f92546c97068cbc167a68f4fbaf8b3a28759
Brockkoli <kianyun8186@gmail.com>
2023-02-27 16:32:01 +0800

    add: formatting and colour code

--- info.py
+++ info.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from whois import whois
-import datetime
+import colorama
+
+colorama.init()
 
 # make a request to the website
 url = "https://www.google.com"
@@ -12,6 +14,7 @@ if response.status_code == 200:
     # parse the HTML content of the website
     soup = BeautifulSoup(response.content, "html.parser")
 
+    print(colorama.Fore.YELLOW + "*" * 30 + colorama.Style.RESET_ALL)
     # get the title of the website
     title = soup.title.string
     print("Title:", title)
@@ -19,16 +22,21 @@ if response.status_code == 200:
     # get the description of the website
     description = soup.find("meta", attrs={"name": "description"})
     if description:
-        print("Description:", description["content"])
+        print("* Description:", description["content"] + "*")
+    
 
     # get the WHOIS information of the website
     domain = url.split("//")[-1].split("/")[0]
     whois_info = whois(domain)
     print("Registrar:", whois_info.registrar)
+    print(colorama.Fore.YELLOW + "*" * 30  + colorama.Style.RESET_ALL)
     creation_date = [date.strftime('%d-%B-%Y') for date in whois_info.creation_date]
     expiration_date = [date.strftime('%d-%B-%Y') for date in whois_info.expiration_date]
-    print("Creation Date:", creation_date)
-    print("Expiration Date:", expiration_date)
+
+    # {:<18}" - left-align the string within an 18-character-wide column
+    # join() method joins the list elements with a comma and space
+    print(colorama.Fore.GREEN + "{:<18} {}".format("Creation Date:", ", ".join(creation_date)))
+    print(colorama.Fore.RED + "{:<18} {}".format("Expiration Date:", ", ".join(expiration_date)))
 else:
     print("Error:", response.status_code)
     
