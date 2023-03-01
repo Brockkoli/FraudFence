3a6df4ba0cd9ad53629586955cb7058dd88ad141
Brockkoli <kianyun8186@gmail.com>
2023-03-01 12:55:12 +0800

    add: colour code

--- headers.py
+++ headers.py
@@ -1,12 +1,17 @@
 import requests
+import colorama
+
+colorama.init()
 
 url = input("Enter website URL: ")
+print(colorama.Fore.YELLOW + "*" * 50 + colorama.Style.RESET_ALL)
 
 try:
     response = requests.head(url)
     headers = response.headers
 
     for header, value in headers.items():
-        print(f"{header}: {value}")
+        print(f"{colorama.Fore.GREEN + header  + colorama.Style.RESET_ALL}: {value}")
+    print(colorama.Fore.YELLOW + "*" * 50 + colorama.Style.RESET_ALL)
 except requests.exceptions.RequestException as e:
     print(f"Error: {e}")
