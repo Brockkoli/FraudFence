a23b0f95a695dfa6bdb40aa07bf37f425ee30793
Brockkoli <kianyun8186@gmail.com>
2023-03-01 13:06:39 +0800

    add: keyboard interuptt

--- mainMenu.py
+++ mainMenu.py
@@ -1,9 +1,10 @@
 from pyfiglet import Figlet
 from termcolor import colored
+
 fraudFence = True
 while fraudFence:
     #must change directory FOR NOW
-    f = open('C:\\Users\\Jevan\\Downloads\\fraudfence4.txt', 'r')
+    f = open('fraudfence.txt', 'r')
     file_contents = f.read()
     print (file_contents)
     f.close()
@@ -14,15 +15,20 @@ while fraudFence:
     print("2. port scan")
     print("3. dns lookup")
     print("4. exit")
-    fraudFence = input("Choose an option to run: ")
-    if fraudFence == "1":
-      print("\nperforming whois...\n")
-    elif fraudFence == "2":
-      print("\nperforming port scan...\n")
-    elif fraudFence == "3":
-      print("\nperforming dns lookup...\n")
-    elif fraudFence == "4":
-      print("\nThank you for using FraudFence and have a safe Internet experience!") 
-      fraudFence = None
-    else:
-       print("\nInvalid input. Please enter a number between 1 and 4\n")
+
+    try:
+      fraudFence = input("Choose an option to run: ")
+      if fraudFence == "1":
+        print("\nperforming whois...\n")
+      elif fraudFence == "2":
+        print("\nperforming port scan...\n")
+      elif fraudFence == "3":
+        print("\nperforming dns lookup...\n")
+      elif fraudFence == "4":
+        print("\nThank you for using FraudFence and have a safe Internet experience!") 
+        fraudFence = None
+      else:
+        print("\nInvalid input. Please enter a number between 1 and 4\n")
+    except KeyboardInterrupt:
+        print("\nExiting program...")
+        exit()
