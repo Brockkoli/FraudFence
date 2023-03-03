2298998c95f2a9118dfe85abf83378b130ee824b
yongjaven <97573389+yongjaven@users.noreply.github.com>
2023-03-03 16:51:01 +0800

    Update mainMenu.py
    Updated the integration for options 4-6

--- mainMenu.py
+++ mainMenu.py
@@ -10,7 +10,7 @@ while fraudFence:
     f.close()
     g = Figlet(font = 'standard')
     print(colored(g.renderText('     FraudFence'),'green'))
-    print("Welcome to Fraudfence. Here are some stuff you can do")
+    print("Welcome to Fraudfence. Here are some options you can perform")
     print("\n1. Whois")
     print("2. Port Scan")
     print("3. DNS Lookup")
@@ -22,28 +22,28 @@ while fraudFence:
     try:
       fraudFence = input("Choose an option to run: ")
       if fraudFence == "1":
-        from info import whois_check
+        from info import whois_check #need to change the import
         print("\nperforming Whois...\n")
       elif fraudFence == "2":
-        from portscan import portscan_check
+        from portscan import portscan_check #need to change the import
         print("\nperforming Port Scan...\n")
       elif fraudFence == "3":
-        from dnslookup import dnslookup_check
+        from dnslookup import dnslookup #need to change the import
         print("\nperforming DNS Lookup...\n")
       elif fraudFence == "4":
-        from locationchecker import serverlocation_check
+        from locationchecker import serverlocationchecker
         print("\nperforming Server Location Checker...\n")
       elif fraudFence == "5":
-        from headers import header_check
+        from headers import headers
         print("\nperforming Web Header Checker...\n")
       elif fraudFence == "6":
-        from sslinformation import ssl_check
+        from sslinformation import ssl_checker
         print("\nperforming SSL Information...\n")
       elif fraudFence == "7":
         print("\nThank you for using FraudFence and have a safe Internet experience!") 
         fraudFence = None
       else:
-        print("\nInvalid input. Please enter a number between 1 and 4\n")
+        print("\nInvalid input. Please enter a number between 1 and 7\n")
     except KeyboardInterrupt:
         print("\nExiting program...")
         exit()
