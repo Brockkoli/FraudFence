c8825067876998546dc9d5bafb3a35e631c8b596
= <94104908+jevancjk@users.noreply.github.com>
2023-03-03 16:06:12 +0800

    Update headers.py
    Make the output neater for Content-Security-Policy

--- headers.py
+++ headers.py
@@ -11,6 +11,10 @@ try:
     headers = response.headers
 
     for header, value in headers.items():
+        #the Content-Security-Policy header displays all the browsers in one line
+        if header == "Content-Security-Policy":
+            #the join function will print each output in one line
+            value = ', \n'.join(value.split())
         print(f"{colorama.Fore.GREEN + header  + colorama.Style.RESET_ALL}: {value}")
     print(colorama.Fore.YELLOW + "*" * 50 + colorama.Style.RESET_ALL)
 except requests.exceptions.RequestException as e:
