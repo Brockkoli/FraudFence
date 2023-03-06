cf3c6cb9f4c046aa0defd9c6c2741e44434104d1
yongjaven <97573389+yongjaven@users.noreply.github.com>
2023-03-06 18:06:11 +0800

    Update headers.py
    Edited the code for integration with mainMenu.py

--- headers.py
+++ headers.py
@@ -1,21 +1,30 @@
-import requests
 import colorama
+import requests
 
-colorama.init()
 
-url = input("Enter website URL: ")
-print(colorama.Fore.YELLOW + "*" * 50 + colorama.Style.RESET_ALL)
-
-try:
-    response = requests.head(url)
-    headers = response.headers
-
-    for header, value in headers.items():
-        #the Content-Security-Policy header displays all the browsers in one line
-        if header == "Content-Security-Policy":
-            #the join function will print each output in one line
-            value = ', \n'.join(value.split())
-        print(f"{colorama.Fore.GREEN + header  + colorama.Style.RESET_ALL}: {value}")
+def headers(url):
+    colorama.init()
     print(colorama.Fore.YELLOW + "*" * 50 + colorama.Style.RESET_ALL)
-except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+
+    try:
+        if not url.startswith("http"):
+            url = "https://" + url
+        response = requests.head(url)
+        headers = response.headers
+
+        for header, value in headers.items():
+            # the Content-Security-Policy header displays all the browsers in one line
+            if header == "Content-Security-Policy":
+                # the join function will print each output in one line
+                value = ', \n'.join(value.split())
+            print(f"{colorama.Fore.GREEN + header + colorama.Style.RESET_ALL}: {value}")
+        print(colorama.Fore.YELLOW + "*" * 50 + colorama.Style.RESET_ALL)
+
+        checker = input("Do you wish to continue? (Y/N) ")
+        if checker == "y":
+            return True
+        else:
+            return False
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
