a1508de56faddf58a5ebdde562859fd17cc6867e
Brockkoli <kianyun8186@gmail.com>
2023-03-07 02:28:57 +0800

    fix: kinda a solution for whois ccTLDs

--- info.py
+++ info.py
@@ -1,89 +1,25 @@
-import colorama
 import requests
-from bs4 import BeautifulSoup
-from whois import whois
+import xml.dom.minidom
+import xml.etree.ElementTree as ET
 
+url = "https://whoisapi-whois-v2-v1.p.rapidapi.com/whoisserver/WhoisService"
+domain = input("Enter the URL domain: ")
 
-def whois_check(url):
-    colorama.init()
+if domain.startswith("https://" or "http://" or "www"):
+        domain = domain.strip("https://")
+        domain = domain.strip("http://")
+        domain = domain.strip("www")
 
-    if not url.startswith("http"):
-        url = "https://" + url
-    response = requests.get(url)
+querystring = {"domainName":f"{domain}","apiKey":"at_SDpAVjCNg3tXpSyM66qqcKkPyJSTJ","outputFormat":"XML","da":"0","ipwhois":"1","thinWhois":"0","_parse":"0","preferfresh":"0","checkproxydata":"0","ip":"1"}
 
-    # check the status code of the response
-    if response.status_code == 200:
-        # parse the HTML content of the website
-        soup = BeautifulSoup(response.content, "html.parser")
+headers = {
+	"X-RapidAPI-Key": "81c0482df8msh514a1a1beae15f7p1637ccjsnfcaa82735744",
+	"X-RapidAPI-Host": "whoisapi-whois-v2-v1.p.rapidapi.com"
+}
 
-        print(colorama.Fore.YELLOW + "*" * 30 + colorama.Style.RESET_ALL)
-        # get the title of the website
-        title = soup.title.string
-        print("Title:", title)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-        # get the description of the website
-        try:
-            description = soup.find("meta", attrs={"name": "description"})
-            if description:
-                print("Description:", description["content"])
-        except:
-            print("Closing socket.")
+dom = xml.dom.minidom.parseString(response.text)
+pretty_xml = dom.toprettyxml()
 
-        # get the WHOIS information of the website
-        domain = url.split("//")[-1].split("/")[0]
-        whois_info = whois(domain)
-
-        print("Registrar:", whois_info.registrar)
-        print(colorama.Fore.YELLOW + "*" * 30 + colorama.Style.RESET_ALL)
-
-        if whois_info.creation_date:
-            try:
-                creation_date = [date.strftime('%d-%B-%Y') for date in whois_info.creation_date]
-                print(colorama.Fore.GREEN + "{:<18} {}".format("Creation Date:", ", ".join(creation_date)))
-            except:
-                creation_date = [whois_info.creation_date.strftime('%d-%B-%Y')]
-                # {:<18}" - left-align the string within an 18-character-wide column
-                # join() method joins the list elements with a comma and space
-                print(colorama.Fore.GREEN + "{:<18} {}".format("Creation Date:", ", ".join(creation_date)))
-        else:
-            print("Creation date not available.")
-
-        if whois_info.expiration_date:
-            try:
-                expiration_date = [date.strftime('%d-%B-%Y') for date in whois_info.expiration_date]
-                print(colorama.Fore.RED + "{:<18} {}".format("Expiration Date:", ", ".join(expiration_date)))
-                print(colorama.Fore.YELLOW + "-" * 30 + colorama.Style.RESET_ALL)
-            except:
-                expiration_date = [whois_info.expiration_date.strftime('%d-%B-%Y')]
-                print(colorama.Fore.RED + "{:<18} {}".format("Expiration Date:", ", ".join(expiration_date)))
-                print(colorama.Fore.YELLOW + "-" * 30 + colorama.Style.RESET_ALL)
-        else:
-            print("Expiration date not available.")
-
-        # Name Servers
-        if whois_info.name_servers:
-            print("Name Servers:")
-            for ns in whois_info.name_servers:
-                print("\t", ns)
-
-        print(colorama.Fore.YELLOW + "-" * 30 + colorama.Style.RESET_ALL)
-
-        # Registrant Information
-        if whois_info.name:
-            print('Registrant name:', whois_info.name)
-
-        if whois_info.email:
-            print('Registrant email:', whois_info.email)
-
-        if whois_info.address:
-            print('Registrant address:', whois_info.address)
-
-        print(colorama.Fore.YELLOW + "-" * 30 + colorama.Style.RESET_ALL)
-
-        checker = input("Do you wish to continue? (Y/N) ")
-        if checker == "y":
-            return True
-        else:
-            return False
-    else:
-        print("Error:", response.status_code)
+print(pretty_xml)
