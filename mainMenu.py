91023e71edb57109b5825eb6564f3db023e46b2e
yongjaven <97573389+yongjaven@users.noreply.github.com>
2023-03-06 18:03:31 +0800

    Update mainMenu.py
    Edited some of the codes for integration. Included a general 'url' so that user does not have to input url for every function. The code will grab the ip address from url name as well.

--- mainMenu.py
+++ mainMenu.py
@@ -1,49 +1,91 @@
+import socket
+
 from pyfiglet import Figlet
 from termcolor import colored
 
+# Importing modules
+from dnslookup import dnslookup
+from headers import headers
+from info import whois_check
+from locationchecker import serverlocationchecker
+from portscan import portscan_check
+from sslinformation import ssl_checker
+
+f = open('fraudfence.txt', 'r')
+file_contents = f.read()
+print(file_contents)
+f.close()
+g = Figlet(font='standard')
+print(colored(g.renderText('     FraudFence'), 'green'))
+url = input("Type in the URL: ")
+
 fraudFence = True
-while fraudFence:
-    #must change directory FOR NOW
-    f = open('fraudfence.txt', 'r')
-    file_contents = f.read()
-    print (file_contents)
-    f.close()
-    g = Figlet(font = 'standard')
-    print(colored(g.renderText('     FraudFence'),'green'))
-    print("Welcome to Fraudfence. Here are some options you can perform")
-    print("\n1. Whois")
+
+
+def printoptions():
+    print("Welcome to Fraudfence. Here are some options you can perform on " + url)
+    print("\nU. Update URL")
+    print("1. Whois")
     print("2. Port Scan")
     print("3. DNS Lookup")
     print("4. Server Location Checker")
     print("5. Web Header Checker")
     print("6. SSL Information")
-    print("7. exit")
+    print("7. exit\n")
 
-    try:
-      fraudFence = input("Choose an option to run: ")
-      if fraudFence == "1":
-        from info import whois_check #need to change the import
-        print("\nperforming Whois...\n")
-      elif fraudFence == "2":
-        from portscan import portscan_check #need to change the import
-        print("\nperforming Port Scan...\n")
-      elif fraudFence == "3":
-        from dnslookup import dnslookup #need to change the import
-        print("\nperforming DNS Lookup...\n")
-      elif fraudFence == "4":
-        from locationchecker import serverlocationchecker
-        print("\nperforming Server Location Checker...\n")
-      elif fraudFence == "5":
-        from headers import headers
-        print("\nperforming Web Header Checker...\n")
-      elif fraudFence == "6":
-        from sslinformation import ssl_checker
-        print("\nperforming SSL Information...\n")
-      elif fraudFence == "7":
-        print("\nThank you for using FraudFence and have a safe Internet experience!") 
-        fraudFence = None
-      else:
-        print("\nInvalid input. Please enter a number between 1 and 7\n")
-    except KeyboardInterrupt:
-        print("\nExiting program...")
+
+def exitoption():
+    if fraudFence is None:
+        print("\nThank you for using FraudFence and have a safe Internet experience!")
         exit()
+
+
+if fraudFence:
+    while fraudFence:
+        printoptions()
+        try:
+            fraudFence = input("Choose an option to run: ")
+            if fraudFence == "u":
+                url = input("Type in the URL: ")
+            if fraudFence == "1":
+                result = whois_check(url)
+                if not result:
+                    fraudFence = None
+                    exitoption()
+            elif fraudFence == "2":
+                result = portscan_check(url)
+                if not result:
+                    fraudFence = None
+                    exitoption()
+            elif fraudFence == "3":
+                result = dnslookup(url)
+                if not result:
+                    fraudFence = None
+                    exitoption()
+            elif fraudFence == "4":
+                url = url.strip("https://")
+                url = url.strip("http://")
+                ip_address = socket.gethostbyname(url)
+                result = serverlocationchecker(ip_address)
+                if not result:
+                    fraudFence = None
+                    exitoption()
+            elif fraudFence == "5":
+                result = headers(url)
+                if not result:
+                    fraudFence = None
+                    exitoption()
+            elif fraudFence == "6":
+                result = ssl_checker(url)
+                if not result:
+                    fraudFence = None
+                    exitoption()
+            elif fraudFence == "7":
+                fraudFence = None
+                exitoption()
+            else:
+                print("\nInvalid input. Please enter a number between 1 and 7\n")
+
+        except KeyboardInterrupt:
+            print("\nExiting program...")
+            exit()
