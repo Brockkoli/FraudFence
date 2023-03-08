bf3470c35d9e9b10aaabe6094d1452ad1c62aaa7
yongjaven <97573389+yongjaven@users.noreply.github.com>
2023-03-08 23:13:33 +0800

    Formatted output

--- info.py
+++ info.py
@@ -1,23 +1,20 @@
-import requests
-import xml.dom.minidom
-import xml.etree.ElementTree as ET
 import colorama
-
-colorama.init()
+import requests
+import xml.etree.ElementTree as ET
 
 def whois_check(domain):
+        colorama.init()
         url = "https://whoisapi-whois-v2-v1.p.rapidapi.com/whoisserver/WhoisService"
-        # domain = input("Enter the URL domain: ")
 
         if domain.startswith("https://"):
                 domain = domain.strip("https://")
         elif domain.startswith("http://"):
                 domain = domain.strip("http://")
-        elif domain.startswith("www"):     
+        elif domain.startswith("www"):
                 domain = domain.strip("www")
-        elif domain.startswith("https://www"):     
+        elif domain.startswith("https://www"):
                 domain = domain.strip("https://www")
-        elif domain.startswith("http://www"):     
+        elif domain.startswith("http://www"):
                 domain = domain.strip("http://www")
 
         querystring = {"domainName":f"{domain}","apiKey":"at_SDpAVjCNg3tXpSyM66qqcKkPyJSTJ","outputFormat":"XML","da":"0","ipwhois":"1","thinWhois":"0","_parse":"0","preferfresh":"0","checkproxydata":"0","ip":"1"}
@@ -29,9 +26,15 @@ def whois_check(domain):
 
         response = requests.request("GET", url, headers=headers, params=querystring)
 
-        dom = xml.dom.minidom.parseString(response.text)
-        pretty_xml = dom.toprettyxml()
-
+        root = ET.fromstring(response.content)
+        stripped_text = root.find(".//strippedText").text
+        print("WHOIS information on: " + colorama.Fore.YELLOW + domain + colorama.Style.RESET_ALL)
         print("-" * 50)
-        print("Whois for: " + colorama.Fore.YELLOW + domain + colorama.Style.RESET_ALL)
-        print(pretty_xml)
+        print("Whois for: " + domain)
+        print(stripped_text)
+
+        checker = input("Do you wish to continue? (Y/N) ")
+        if checker == "y":
+                return True
+        else:
+                return False
