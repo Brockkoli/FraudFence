07b1af18c0ab1872fe79b2016a07e0170c713f4e
= <94104908+jevancjk@users.noreply.github.com>
2023-03-10 21:18:37 +0800

    Update gui_sslinformation.py

--- gui_codes/gui_sslinformation.py
+++ gui_codes/gui_sslinformation.py
@@ -5,7 +5,7 @@ import textwrap
 import PySimpleGUI as sg
 
 def print_cert_info(cert: dict):
-    #create an empty list then append to it once the values have been scraped
+    # create an empty list then append to it once the values have been scraped
     rows = []
     for key, value in cert.items():
         str_value = str(value)
@@ -33,16 +33,15 @@ def print_cert_info(cert: dict):
 
 def ssl_checker(url):
     if url.startswith("https://" or "http://"):
-        #remove the http/https scheme from the url
+        # remove the http/https scheme from the url
         url = url.strip("https://")
         url = url.strip("http://")
 
     if not url.startswith("www."):
-        #append the www prefix to the url
+        # append the www prefix to the url
         newurl = "www." + url
         url = newurl
 
-    cert_info = ""
     try:
         context = ssl.create_default_context()
         with socket.create_connection((url, 443)) as sock:
