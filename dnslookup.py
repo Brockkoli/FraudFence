ff9b07516233d31dd47d5b196c9c2c398c231eb4
yongjaven <97573389+yongjaven@users.noreply.github.com>
2023-03-12 01:23:53 +0800

    Added a dictionary to store the return result
    Added a few other information as well.

--- dnslookup.py
+++ dnslookup.py
@@ -1,7 +1,6 @@
 import colorama
 import dns.resolver
 
-
 def dnslookup(domain):
     if domain.startswith("https://"):
         domain = domain.strip("https://")
@@ -16,14 +15,18 @@ def dnslookup(domain):
 
     colorama.init()
 
+    dns_records = {}
+
     print("-" * 50)
     print("DNS lookup for " + colorama.Fore.GREEN + domain + "\n" + colorama.Style.RESET_ALL)
 
     # get A record
     try:
         A_record = dns.resolver.resolve(domain, 'A')
+        dns_records['A'] = []
         for ip in A_record:
             print('A Record:', ip.to_text())
+            dns_records['A'].append(ip.to_text())
     except:
         print("No A record found.")
 
@@ -32,8 +35,10 @@ def dnslookup(domain):
     # get AAAA record
     try:
         AAAA_record = dns.resolver.resolve(domain, 'AAAA')
+        dns_records['AAAA'] = []
         for ip in AAAA_record:
             print('AAAA Record:', ip)
+            dns_records['AAAA'].append(ip.to_text())
     except:
         print("No AAAA record found.")
 
@@ -42,8 +47,10 @@ def dnslookup(domain):
     # get ANY record
     try:
         ANY_record = dns.resolver.resolve(domain, 'ANY')
+        dns_records['ANY'] = []
         for record in ANY_record:
             print('ANY Record:', record)
+            dns_records['ANY'].append(record.to_text())
     except:
         print("No ANY record found.")
 
@@ -52,8 +59,10 @@ def dnslookup(domain):
     # get CAA record
     try:
         CAA_record = dns.resolver.resolve(domain, 'CAA')
+        dns_records['CAA'] = []
         for record in CAA_record:
             print('CAA Record:', record)
+            dns_records['CAA'].append(record.to_text())
     except:
         print("No CAA record found.")
 
@@ -62,8 +71,10 @@ def dnslookup(domain):
     # get CNAME record
     try:
         CNAME_record = dns.resolver.resolve(domain, 'CNAME')
+        dns_records['CNAME'] = []
         for record in CNAME_record:
             print('CNAME Record:', record)
+            dns_records['CNAME'].append(record.to_text())
     except:
         print("No CNAME record found.")
 
@@ -72,8 +83,10 @@ def dnslookup(domain):
     # get MX record
     try:
         MX_record = dns.resolver.resolve(domain, 'MX')
+        dns_records['MX'] = []
         for record in MX_record:
             print('MX Record:', record)
+            dns_records['MX'].append(record.to_text())
     except:
         print("No MX record found.")
 
@@ -82,20 +95,62 @@ def dnslookup(domain):
     # get NS record
     try:
         NS_record = dns.resolver.resolve(domain, 'NS')
+        dns_records['NS'] = []
         for record in NS_record:
             print('NS Record:', record)
+            dns_records['NS'].append(record.to_text())
     except:
         print("No NS record found.")
+
+    print(colorama.Fore.YELLOW + "-" * 40 + colorama.Style.RESET_ALL)
+
+    # get PTR record
+    try:
+        PTR_record = dns.resolver.resolve(domain, 'PTR')
+        dns_records['PTR'] = []
+        for record in PTR_record:
+            print('PTR Record:', record)
+            dns_records['PTR'].append(record.to_text())
+    except:
+        print("No PTR record found.")
+
+    print(colorama.Fore.YELLOW + "-" * 40 + colorama.Style.RESET_ALL)
+
+    # get SOA record
+    try:
+        SOA_record = dns.resolver.resolve(domain, 'SOA')
+        dns_records['SOA'] = []
+        for record in SOA_record:
+            print('SOA Record:', record)
+            dns_records['SOA'].append(record.to_text())
+    except:
+        print("No SOA record found.")
+
+    print(colorama.Fore.YELLOW + "-" * 40 + colorama.Style.RESET_ALL)
+
+    # get SRV record
+    try:
+        SRV_record = dns.resolver.resolve(domain, 'SRV')
+        dns_records['SRV'] = []
+        for record in SRV_record:
+            print('SRV Record:', record)
+            dns_records['SRV'].append(record.to_text())
+    except:
+        print("No SRV record found.")
 
     print(colorama.Fore.YELLOW + "-" * 40 + colorama.Style.RESET_ALL)
 
     # get TXT record
     try:
         TXT_record = dns.resolver.resolve(domain, 'TXT')
+        dns_records['TXT'] = []
         for record in TXT_record:
             print('TXT Record:', record)
+            dns_records['TXT'].append(record.to_text())
     except:
         print("No TXT record found.")
 
     print(colorama.Fore.YELLOW + "-" * 40 + colorama.Style.RESET_ALL)
+
+    return dns_records
 
